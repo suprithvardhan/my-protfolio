acba9e8c4e953808f815d01553d62f325668558c
suprithvardhan <127759820+suprithvardhan@users.noreply.github.com>
2024-05-23 22:59:08 +0530

    Update Contact.tsx
    removed the event listener which are used to add transition effect to the texts

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -4,30 +4,6 @@ import styles from './Contact.module.css';
 import { FaTwitter, FaInstagram, FaLinkedin, FaGithub, FaCodepen } from 'react-icons/fa';
 
 const Contact = () => {
-  useEffect(() => {
-    const icons = document.querySelectorAll(`.${styles.icon}`);
-
-    icons.forEach(icon => {
-      const handleMouseMove = (e: MouseEvent) => {
-        const rect = icon.getBoundingClientRect();
-        const x = e.clientX - rect.left;
-        const y = e.clientY - rect.top;
-        (icon as HTMLElement).style.transform = `translate(${x * 0.1}px, ${y * 0.1}px)`;
-      };
-
-      const handleMouseLeave = () => {
-        (icon as HTMLElement).style.transform = 'translate(0, 0)';
-      };
-
-      icon.addEventListener('mousemove', handleMouseMove);
-      icon.addEventListener('mouseleave', handleMouseLeave);
-
-      return () => {
-        icon.removeEventListener('mousemove', handleMouseMove);
-        icon.removeEventListener('mouseleave', handleMouseLeave);
-      };
-    });
-  }, []);
 
   return (
     <div className={styles.contactContainer}>
